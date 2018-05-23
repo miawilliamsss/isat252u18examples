6faad99a3c72d548e787ebebe6c8a63332441978
Morgan Benton <morgan.benton@gmail.com>
2018-05-23 11:34:06 -0400

    student props and methods

--- js/lms/student.js
+++ js/lms/student.js
@@ -2,9 +2,13 @@
 // the Student class
 
 function Student(props) {
+  props = props || {};
   this.first_name = props.first_name || "";
   this.last_name  = props.last_name  || "";
   this.courses    = [];
+  this.enroll = function(course) {
+    this.courses.push(course);
+  };
 }
 
 module.exports = Student;
--- js/lms/student.spec.js
+++ js/lms/student.spec.js
@@ -2,7 +2,9 @@
 const Student = require('./student');
 
 describe("A Student", () => {
-
+  /*
+    Properties (Nouns)
+   */
   it("has a first name", () => {
     let oscar = new Student({ first_name: "Oscar" });
     expect(oscar.first_name).toBeDefined();
@@ -16,8 +18,45 @@ describe("A Student", () => {
   });
 
   it("has courses", () => {
-    let oscar = new Student({ first_name: "Oscar", last_name: "Mayer" });
+    let oscar = new Student({
+      first_name: "Oscar", 
+      last_name: "Mayer"
+    });
     expect(oscar.courses).toBeDefined();
     expect(Array.isArray(oscar.courses)).toBe(true);
+  });
+
+  /*
+    Methods (Verbs)
+   */
+  it("can enroll in a course", () => {
+    let oscar = new Student({
+      first_name: "Oscar", 
+      last_name: "Mayer"
+    });
+    // create a mock(fake) instructor
+    let mockProf = {
+      first_name: "Morgan",
+      last_name: "Benton",
+      office: "ISAT/CS 124",
+      email: "bentonmc@jmu.edu",
+      greet: () => { console.log('Yo!'); }
+    };
+    // create a mock(fake) version of a course
+    let mock252 = {
+      name: "Programming and Problem Solving",
+      program: "ISAT",
+      number: "252",
+      location: "ISAT/CS 337",
+      instructor: mockProf
+    };
+
+    let num_enrolled = oscar.courses.length;
+    oscar.enroll(mock252);
+    expect(oscar.courses.length).toBe(num_enrolled + 1);
+    expect(oscar.courses[num_enrolled].name)
+      .toBe("Programming and Problem Solving");
+    expect(oscar.courses[num_enrolled].instructor.first_name)
+      .toBe("Morgan");
   });
 });
